2d750dfcc7a3c529b909727228af14bc8dfc4c6c
Shivam Patel <87613778+ShivamPatel-24@users.noreply.github.com>
2023-10-02 18:34:50 -0400

    add leaderboard page

--- app.js
+++ app.js
@@ -7,15 +7,21 @@ const morgan = require("morgan");
 port = process.env.PORT || 3000;
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use(morgan("dev"));
-app.use(express.json());
 
 mongoose.connect(process.env.DB_LOCAL_URL).then(() => {
     console.log("DB connection established");
 });
 
 app.get('/', (req, res) => {
-    res.send("Memory matching game")
+    res.send("Match Making Game")
+})
+
+app.get('/leaderboard', (req, res) => {
+    res.render("leaderboard")
 })
 
 app.listen(port, () => {
